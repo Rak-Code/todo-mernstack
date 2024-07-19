149e1e99f9ab1736b8f9a1b544f43630dc05abe0
Rakesh Gupta <rakeshgupta1361411@gmail.com>
2024-07-19 22:49:51 +0530

    Done

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/todolist');
+
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  completed: Boolean,
+});
+
+const Task = mongoose.model('Task', taskSchema);
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post('/tasks', async (req, res) => {
+  const task = new Task(req.body);
+  await task.save();
+  res.json(task);
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Task deleted' });
+});
+
+app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
